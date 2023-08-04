7163a7b556deb2859a50340e40998354d9cf4bcb
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2023-08-05 00:16:54 +0530

    built:completed

---
+++ code_with_harry/error_handling/index.js
@@ -0,0 +1,13 @@
+setTimeout(() => {
+    console.log("ABS");
+}, 1000);
+
+try {
+    console.log(paarth);
+} catch (err) {
+    console.log(err);
+}
+
+setTimeout(() => {
+    console.log("ABS2");
+}, 2000);
