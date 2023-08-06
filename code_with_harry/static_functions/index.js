4a5f46dc0c2088947fee7d0368225cc2ee628d68
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2023-08-07 00:39:30 +0530

    style:alignment

--- code_with_harry/static_functions/index.js
+++ code_with_harry/static_functions/index.js
@@ -1,15 +1,15 @@
-class Animal{
-    constructor(name){
+class Animal {
+    constructor(name) {
         this.name = Animal.capitalize(name);
     }
-    walk(){
-        console.log("Animal "+Animal.capitalize(this.name)+" is walking");
+    walk() {
+        console.log("Animal " + Animal.capitalize(this.name) + " is walking");
     }
-    static capitalize(name){
-        return name.charAt(0).toUpperCase() + name.substr(1,name.length)
+    static capitalize(name) {
+        return name.charAt(0).toUpperCase() + name.substr(1, name.length)
     }
 }
-let j = new Animal("jack") 
+let j = new Animal("jack")
 j.walk();
 
 // console.log(j.capitalize("thisa"));//this doesn't work with static functions
