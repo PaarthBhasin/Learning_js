1e72f0e61ff376b87757e6961f38e0033ea7d745
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2023-08-06 21:49:57 +0530

    build:notes_app_completed

---
+++ code_with_harry/ch_10_prob/index.js
@@ -0,0 +1,9 @@
+// Notes app
+// let a = prompt("Enter your note");
+// localStorage.setItem("notes",a);
+// let b = localStorage.getItem("notes");
+// console.log(b)
+let c = confirm("Do you want to delete your notes?");
+if(c){
+    localStorage.removeItem();
+}
