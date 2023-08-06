8b90acf12bf3d65747d1a6715e23bd4492569603
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2023-08-06 21:42:38 +0530

    build:stage1_complete

---
+++ code_with_harry/hackathon_website/index.js
@@ -0,0 +1,25 @@
+let url = "https://kontests.net/api/v1/all"
+let response=fetch(url);
+response.then((v)=>{
+    return v.json();
+}).then((contests)=>{
+    console.log(contests);
+    ihtml="";
+    for(item in contests){
+        console.log(contests[item]);
+        ihtml+=`
+        <div class="card" style="width: 22rem;">
+                <img src="https://cdn.mos.cms.futurecdn.net/3eSHhTo4WUYwXJUpMCxEUo-1920-80.jpg.webp" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${contests[item].name}</h5>
+                  <p class="card-text">Status is : ${contests[item].status} and site is : ${contests[item].site}</p>
+                  <p class="card-text">In 24 hours : ${contests[item].in_24_hours}</p>
+                  <p>Start at: ${contests[item].start_time}</p>
+                  <p>End at: ${contests[item].end_time}</p>
+                  <a href="${contests[item].url}" class="btn btn-primary">Visit Contest</a>
+                </div>
+              </div>
+          </div>`
+    }
+        cardContainer.innerHTML=ihtml;
+})
