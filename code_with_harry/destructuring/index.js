ddde9178f905e57fcf3ded6ced1e67b4af393408
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2023-08-08 00:01:01 +0530

    build:incomplete,spread operator left

---
+++ code_with_harry/destructuring/index.js
@@ -0,0 +1,12 @@
+let arr = [3, 5, 2, 4, 6, 3, 7, 9]
+// let [a,b,c,d,e,...rest]= arr;//destructuring: a=3,b=5
+// console.log(a,b,c,d,rest)
+// let [a, , , , ...rest] = arr;
+// console.log(a, rest);//3 [6, 3, 7, 9]
+let {
+    a,
+    b
+} = {
+    a: 1,
+    b: 5
+};
