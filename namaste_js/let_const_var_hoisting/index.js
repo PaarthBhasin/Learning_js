2de78e3e3a893327c1db83be758b61368762363e
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2023-09-26 23:36:36 +0530

    closures

--- namaste_js/let_const_var_hoisting/index.js
+++ namaste_js/let_const_var_hoisting/index.js
@@ -1,5 +1,41 @@
-let a = 20;
-function x(){
-    var a = 10;
+// function x(){
+//     let a = 7;
+//     function y(){
+//         console.log(a);
+//     }
+//     return y;
+//     // return function y(){
+//     //     console.log(a);
+//     // } means the same as above
+// }
+// var b = x();
+// console.log(b)
+// b();
+
+
+
+
+// function x(){
+//     let a = 7;
+//     function y(){
+//         console.log(a);
+//     }
+//     a=100;
+//     return y;//will return 100 now
+// }
+// var b = x();
+// console.log(b)
+// b();
+
+function z(){
+    function x(){
+        let a = 7;
+        function y(){
+            console.log(a);
+        }
+        y();
+    }
+    x();
 }
-x();
+z();
+// it forms closures for x and z also
