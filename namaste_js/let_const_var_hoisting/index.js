71340dc11a404113dacfca91fe2925931eb07c20
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2023-10-05 19:38:04 +0530

    closure practice

--- namaste_js/let_const_var_hoisting/index.js
+++ namaste_js/let_const_var_hoisting/index.js
@@ -1,32 +1,8 @@
-const users = [
-    {
-        firstName: "paarth",
-        lastName: "bhasin",
-        age:'21'
-    },
-    {
-        firstName: "pankaj",
-        lastName: "bhasin",
-        age:'61'
-    },
-    {
-        firstName: "prakhar",
-        lastName: "bhasin",
-        age:'27'
-    },
-    {
-        firstName: "guljeet",
-        lastName: "kaur",
-        age:'52'
-    },
-
-]
-
-const output = users.reduce(function (acc,curr){
-    if(curr.age<30){
-        acc.push(curr.firstName);
+function outer(){
+    var a = 10;
+    function inner(a){
+        console.log(a);
     }
-    return acc;
-},[])
-
-console.log(output);
+    return inner;
+}
+outer()(100);
