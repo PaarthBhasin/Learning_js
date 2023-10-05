cc375cd274c9d85463c03e6a9063583f221e4d55
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2023-10-05 20:22:55 +0530

    filter function example

--- namaste_js/let_const_var_hoisting/index.js
+++ namaste_js/let_const_var_hoisting/index.js
@@ -1,8 +1,4 @@
 const radii = [3,1,2,4];
-const area = function(radius){
-    return Math.PI * radius * radius;
-}
-console.log(radii.map(function(radius){
-    return Math.PI * radius * radius;
-}));
-console.log(radiuss.map(area));
+console.log(radii.filter(function (radius){
+    return radius<3;
+}))
