c83f689c8ebf24ec5b45760ae2afc20c94e40076
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2023-11-24 23:38:37 +0530

    q5 control statement

--- kirat_cohort_prep/index.js
+++ kirat_cohort_prep/index.js
@@ -1,12 +1,35 @@
-function gradeCal(){
-    const marks = document.getElementById("marksId").value;
+function dayNum(){
+    const num = document.getElementById("dayId").value;
+    const n = parseFloat(num);
 
-    const m = parseFloat(marks);
-
-    if(m >= 90 && m <= 100){
-        console.log("Your grade is A")
+    switch(n){
+        case 1: 
+        write("Sunday");
+        break;
+        case 2:
+            write("Monday");
+             break;
+        case 3: 
+            write("Tuesday");
+            break;
+        case 4: 
+            write("Wednesday");
+            break;
+        case 5: 
+            write("Thursday");
+            break;
+        case 6: 
+            write("Friday");
+            break;
+        case 7: 
+            write("Saturday");
+            break;
     }
 }
 document.getElementById("myForm").addEventListener("submit", function(event){
     event.preventDefault();
 })
+
+function write(p){
+    document.getElementById("ans").textContent = p;
+}
