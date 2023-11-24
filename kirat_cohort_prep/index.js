25e688426c71d757c5965eddead0d78b113d5557
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2023-11-24 19:26:41 +0530

    q6 data type

--- kirat_cohort_prep/index.js
+++ kirat_cohort_prep/index.js
@@ -1,16 +1,40 @@
-function calculate(){
-    const len = document.getElementById("len").value
-    const bre = document.getElementById("bre").value
+function inputNumbers() {
+    const numberOfNumbersInput = document.getElementById('numberOfNumbers');
+    const numberOfNumbers = parseInt(numberOfNumbersInput.value);
 
-    const l = parseFloat(len);
-    const b = parseFloat(bre);
+    if (isNaN(numberOfNumbers) || numberOfNumbers <= 0) {
+      alert('Please enter a valid positive integer for n.');
+      return;
+    }
 
-    const area = l*b;
-    const peri = 2*(l+b);
+    const inputContainer = document.getElementById('inputContainer');
+    inputContainer.style.display = 'block';
 
-    const ans = document.getElementById("ans");
-    ans.textContent = `The area of the rectangle is : ${area} and the perimter is : ${peri}`;
-}
-document.getElementById("myForm").addEventListener("submit", function(event){
-    event.preventDefault();
-})
+    const numbersInput = document.getElementById('numbersInput');
+    numbersInput.innerHTML = '';
+
+    for (let i = 0; i < numberOfNumbers; i++) {
+      const inputField = document.createElement('input');
+      inputField.type = 'number';
+      inputField.placeholder = `Enter number ${i + 1}`;
+      numbersInput.appendChild(inputField);
+    }
+  }
+
+  function calculateAverage() {
+    const inputFields = document.querySelectorAll('#numbersInput input');
+    const numberOfNumbers = inputFields.length;
+
+    let sum = 0;
+
+    for (const inputField of inputFields) {
+      sum += parseFloat(inputField.value) || 0;
+    }
+
+    const average = numberOfNumbers > 0 ? sum / numberOfNumbers : 0;
+
+    const logarithm = numberOfNumbers > 0 ? Math.log(average) : 0 ;
+
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = `The average of the entered numbers is: ${logarithm.toFixed(2)}`;
+  }
