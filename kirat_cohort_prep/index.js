29cc2d060e37afbfc345200cb3888f2eb52fde05
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2023-12-10 12:09:32 +0530

    greeting function assignment

--- kirat_cohort_prep/index.js
+++ kirat_cohort_prep/index.js
@@ -1,4 +1,15 @@
-function hello(name){
-    console.log(`Hello ${name}`);
+function greeting(time){
+    if(time>=6 && time<12){
+        console.log("Good morning!");
+    }
+    else if(time>=12 && time<16){
+        console.log("Good afternoon!");
+    }
+    else if(time>16 && time<20){
+        console.log("Good evening!");
+    }
+    else{
+        console.log('Good night!')
+    }
 }
-hello('Paarth')
+greeting(1)
