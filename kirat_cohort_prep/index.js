71aec60ce39dce5b788346f05f54104644d51d70
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2023-12-10 11:27:21 +0530

    bitwise operation assignment

--- kirat_cohort_prep/index.js
+++ kirat_cohort_prep/index.js
@@ -1,35 +1,3 @@
-function dayNum(){
-    const num = document.getElementById("dayId").value;
-    const n = parseFloat(num);
-
-    switch(n){
-        case 1: 
-        write("Sunday");
-        break;
-        case 2:
-            write("Monday");
-             break;
-        case 3: 
-            write("Tuesday");
-            break;
-        case 4: 
-            write("Wednesday");
-            break;
-        case 5: 
-            write("Thursday");
-            break;
-        case 6: 
-            write("Friday");
-            break;
-        case 7: 
-            write("Saturday");
-            break;
-    }
-}
-document.getElementById("myForm").addEventListener("submit", function(event){
-    event.preventDefault();
-})
-
-function write(p){
-    document.getElementById("ans").textContent = p;
-}
+let a = 1;
+let b = 3;
+console.log(a^b);
