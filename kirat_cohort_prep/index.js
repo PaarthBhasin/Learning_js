3853bdebd2ba20e44dba58d33c5be9f58418e9f7
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2023-12-10 14:02:02 +0530

    uppercase function assignment

--- kirat_cohort_prep/index.js
+++ kirat_cohort_prep/index.js
@@ -1,11 +1,9 @@
-function a (arr, callback){
-    const modifiedArray = arr.map(callback);
-    return modifiedArray;
+function transform(str){
+    const uStr = str.map((s)=>{
+        return s.toUpperCase();
+    })
+    return uStr;
 }
-const originalArray = [1,2,3,4];
-
-const doubleCallback = function (element){
-    return element*2;
-}
-const doubleArray = a(originalArray, doubleCallback);
-console.log(doubleArray);
+const arr = ["paarth", "ginni","neha", "pankaj"]
+const ans = transform(arr);
+console.log(ans)
