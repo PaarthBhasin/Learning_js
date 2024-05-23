3669cd212fb9ff9733714370d874f5489dd2a42c
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2024-05-23 14:14:29 +0530

    state example

--- kirat_cohort/week-6-experiments/src/App.jsx
+++ kirat_cohort/week-6-experiments/src/App.jsx
@@ -1,21 +1,39 @@
 import { useEffect, useState } from "react";
+import axios from "axios"
 
 function App(){
-  const [todos, setTodos] = useState([]);
-  
-  useEffect(()=>{
-    fetch("https://sum-server.100xdevs.com/todos")
-    .then( async (res) =>{
-      const json = await res.json();
-      setTodos(json.todos);
-    })
-  }, []);
+  const [selectedId, setSelectedId] = useState(1);
 
-  return(
+  return (
     <div>
-      {todos.map(todo=> <li key={todo.id}>{todo.title} : {todo.description}</li>)}
+      <button onClick={()=> setSelectedId(1)}>1</button>
+      <button onClick={()=> setSelectedId(2)}>2</button>
+      <button onClick={()=> setSelectedId(3)}>3</button>
+      <button onClick={()=> setSelectedId(4)}>4</button>
+      <Todo id={selectedId} />
     </div>
   )
 }
+function Todo({id}){
+  const [todo, setTodo] = useState({});
+
+  useEffect(()=>{
+      axios.get("https://sum-server.100xdevs.com/todos?id="+id)
+      .then((res) =>{
+        setTodo(res.data.todos);
+        console.log(res.data.todos)
+    }).catch((error)=>{
+      console.error("Error fetching data:" , error);
+    })
+  }, [id])
+  return(
+    <div>
+      Id: {id}
+      <h1>{todo.title}</h1> 
+       <h3>{todo.description}</h3>
+    </div>
+  )
+}
+
 
 export default App;
