c72e2f8f904e497959dd4f3c932c88fcf63b705d
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2024-06-18 21:41:23 +0530

    week 7.3 async data queries

--- kirat_cohort/week-6-experiments/src/App.jsx
+++ kirat_cohort/week-6-experiments/src/App.jsx
@@ -1,5 +1,6 @@
-import { RecoilRoot, useRecoilValue } from "recoil"
-import { jobsAtom, messagingAtom, networkAtom, notificationsAtom, totalNotificationSelector } from "./atoms"
+import { RecoilRoot, useRecoilValue } from 'recoil'
+import { notifications, totalNotificationSelector } from './atoms'
+
 
 function App() {
   return <RecoilRoot>
@@ -8,22 +9,17 @@ function App() {
 }
 
 function MainApp() {
-  const networkNotificationCount = useRecoilValue(networkAtom)
-  const jobsNotificationCount = useRecoilValue(jobsAtom);
-  const messagingNotificationCount = useRecoilValue(messagingAtom);
-  const notificationNotificationCount = useRecoilValue(notificationsAtom);
-  const totalNotificationCount = useRecoilValue(totalNotificationSelector)
+  const networkCount = useRecoilValue(notifications)
+  const totalNotificationCount = useRecoilValue(totalNotificationSelector);
+
   return (
     <>
       <button>Home</button>
-      <button>
-        My network (
-        {networkNotificationCount >= 100 ? "99+" : networkNotificationCount})
-      </button>
-      <button>Jobs ( {jobsNotificationCount >= 100 ? "99+" : jobsNotificationCount}))</button>
-      <button>Messaging ( {messagingNotificationCount >= 100 ? "99+" : messagingNotificationCount}))</button>
-      <button>Notifications ( {notificationNotificationCount >= 100 ? "99+" : notificationNotificationCount}))</button>
-      <button>Me({totalNotificationCount})</button>
+      <button>My network ({networkCount.network >= 100 ? "99+" : networkCount.network})</button>
+      <button>Jobs {networkCount.jobs}</button>
+      <button>Messaging ({networkCount.messaging})</button>
+      <button>Notifications ({networkCount.notifications})</button>
+      <button>Me ({totalNotificationCount})</button>
     </>
   )
 }
