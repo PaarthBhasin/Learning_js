61a7718a1b58da19e8828fbb514aa9015b581fd7
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2024-06-19 09:24:34 +0530

    week 7.3 selector family

--- kirat_cohort/week-6-experiments/src/App.jsx
+++ kirat_cohort/week-6-experiments/src/App.jsx
@@ -1,33 +1,17 @@
 
 import './App.css'
-import { RecoilRoot, useRecoilState, useSetRecoilState } from 'recoil';
+import { RecoilRoot, useRecoilValue } from 'recoil';
 import { todosAtomFamily } from './atoms';
-import { useEffect } from 'react';
 
 function App() {
   return <RecoilRoot>
-    <UpdaterComponent />
     <Todo id={1}/>
     <Todo id={2} />
   </RecoilRoot>
 }
 
-function UpdaterComponent(){
-  const updateTodo = useSetRecoilState(todosAtomFamily(2));
-
-  useEffect(()=>{
-   setTimeout(()=>{
-     updateTodo({
-       id: "2",
-       title: "New title",
-       description: "new todo"
-     })
-   }, 5000);
-  })
-}
-
 function Todo({id}) {
-   const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+   const todo = useRecoilValue(todosAtomFamily(id));
 
   return (
     <>
