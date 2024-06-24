ede24e47a11c44761e4282f29dbf19b55500eee8
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2024-06-24 17:57:58 +0530

    week 9.1- useIsOnline, useMousePointer, useWindowDimension hooks

--- kirat_cohort/week-6-experiments/src/App.jsx
+++ kirat_cohort/week-6-experiments/src/App.jsx
@@ -1,75 +1,33 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import { useIsOnline } from "./hooks/useIsOnline";
+import { useWindowDimension } from "./hooks/useWindowDimension";
 
-// function App() {
-//   const { todos, loading } = useTodos(5)
 
-//   return loading ? (
-//     <div> Loading...</div>
-//   ) : (
+// useIsOnline()
+// function App(){
+//     const isOnline = useIsOnline();
+//     if(isOnline){
+//         return "You are online yay!!!"
+//     }
+//     return "You are offline, please connect to the internet"
+// }
+
+// useMousePointer()
+// function App(){
+//     const mousePointer = useMousePointer();
+//    return (
 //     <>
-//       {todos.map((todo, index) => (
-//         <Track key={index} todo={todo} />
-//       ))}
+//     Your mouse position is : {mousePointer.x} : {mousePointer.y}
 //     </>
-//   )
+//    )
 // }
 
-// function Track({ todo }) {
-//   return (
-//     <div>
-//       {todo.title}
-//       <br />
-//       {todo.description}
-//     </div>
-//   )
-// }
+function App() {
+    const windowDimensions = useWindowDimension();
 
-// const useTodos = (n) => {
-//   const [todos, setTodos] = useState([])
-//   const [loading, setLoading] = useState(true)
-
-//   useEffect(() => {
-//     const fetchTodos = () => {
-//       axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
-//         setTodos(res.data.todos)
-//         setLoading(false)
-//       })
-//     }
-//     fetchTodos();
-//     const value = setInterval(fetchTodos, [n * 1000])
-
-//     return () => {
-//       clearInterval(value)
-//     }
-//   }, [n])
-// //   here, fetchTodos will be executed first. And then, we have set an interal where it will be executed again
-// // and finally, we clear our interval
-
-//   return { todos, loading }
-// }
-
-// SWR approach:
-import useSWR from 'swr'
-
-const fetcher = async function (url){
-    const data = await fetch(url);
-    const json = await data.json();
-    return json;
-};
-
-function App(){
-    const {data, error, isLoading} = useSWR("https://sum-server.100xdevs.com/todos", fetcher);
-
-    if(error){
-        return <div>failed to load</div>
-    }
-    if(isLoading){
-        return <div>Loading...</div>
-    }
-    return <div>Hello, you have {data.todos.length} todos!</div>
+    return(
+        <>
+        Your window dimensions are: {windowDimensions.height} : {windowDimensions.width}</>
+    )
 }
 
-
-
-export default App
+export default App;
