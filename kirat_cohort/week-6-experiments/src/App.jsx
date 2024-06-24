02c9754174e5df705c7db338f267ffbe4c720bb3
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2024-06-24 16:00:16 +0530

    week 9.1- types of components in reactjs

--- kirat_cohort/week-6-experiments/src/App.jsx
+++ kirat_cohort/week-6-experiments/src/App.jsx
@@ -1,9 +1,47 @@
-import { RevenueCard } from "./assets/RevenueCard";
+// import { useState } from "react";
+import React from "react";
 
 function App(){
-    return <div className="grid grid-cols-4">
-        <RevenueCard title={"Amount pending"} amount={"92,312.20"} orderCount={"13"}/>
+    return <div>
+        <MyComponent />
     </div>
 }
+
+// Class based component
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { count: 0 };
+    }
+  
+    incrementCount = () => {
+      this.setState({ count: this.state.count + 1 });
+    }
+  
+    render() {
+      return (
+        <div>
+          <p>{this.state.count}</p>
+          <button className="border border-solid text-white bg-black rounded-lg" onClick={this.incrementCount}>Increment</button>
+        </div>
+      );
+    }
+  }
+
+
+// function based component
+// function MyComponent(){
+//     const [count, setCount]= useState(0);
+
+//     const incrementCount = () =>{
+//         setCount(count+1);
+//     }
+//     return(
+//         <div>
+//             <p>{count}</p>
+//             <button className="border border-solid" onClick={incrementCount}>Increment</button>
+//         </div>
+//     )
+// }
 
 export default App;
